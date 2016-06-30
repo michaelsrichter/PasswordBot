ecfb1ec6169ba06d530ec0b5533853c6458275e9
Mike Richter <richtercloud@outlook.com>
2016-06-30 17:36:45 -0400

    started work

--- dx.misv.passwordbot.app/Controllers/MessagesController.cs
+++ dx.misv.passwordbot.app/Controllers/MessagesController.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Connector.Utilities;
 
 namespace dx.misv.passwordbot.app.Controllers
 {
@@ -9,34 +12,29 @@ namespace dx.misv.passwordbot.app.Controllers
     public class MessagesController : ApiController
     {
         /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
+        ///     POST: api/Messages
+        ///     Receive a message from a user and reply to it
         /// </summary>
-        public async Task<Message> Post([FromBody]Message message)
+        public async Task<Message> Post([FromBody] Message message)
         {
             if (message.Type == "Message")
             {
-                // calculate something for us to return
-                var length = (message.Text ?? string.Empty).Length;
-
                 // return our reply to the user
-                return message.CreateReplyMessage($"You sent {length} characters");
+                return await Conversation.SendAsync(message, () => new EchoDialog());
             }
-            else
-            {
-                return HandleSystemMessage(message);
-            }
+            return HandleSystemMessage(message);
         }
 
+        
         private Message HandleSystemMessage(Message message)
         {
             if (message.Type == "Ping")
             {
-                Message reply = message.CreateReplyMessage();
+                var reply = message.CreateReplyMessage();
                 reply.Type = "Ping";
                 return reply;
             }
-            else if (message.Type == "DeleteUserData")
+            if (message.Type == "DeleteUserData")
             {
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
@@ -60,4 +58,117 @@ namespace dx.misv.passwordbot.app.Controllers
             return null;
         }
     }
+
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<Message> argument)
+        {
+            var message = await argument;
+
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    AfterResetAsync,
+                    "Are you sure you want to reset the count?",
+                    "Didn't get that!",
+                    promptStyle: PromptStyle.None);
+            }
+            else
+            {
+                await context.PostAsync(string.Format("{0}: You said {1}", count++, message.Text));
+
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                count = 1;
+
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+
+        [LuisModel("b5f21646-095a-44c5-bce6-87b4a46b28f9", "7687843975f7495d9fb5471a2ac983bc")]
+        [Serializable]
+        public class PasswordDialog : IDialog<object>
+        {
+            protected int count = 1;
+
+            public async Task StartAsync(IDialogContext context)
+            {
+                context.Wait(MessageReceivedAsync);
+            }
+
+            public async Task GetPassword(IDialogContext context, LuisResult result)
+            {
+                EntityRecommendation s;
+
+                if (result.TryFindEntity("password::strong", out passwordType))
+                {
+                }
+            else if (result.TryFindEntity("password::simple", out passwordType))
+                {
+                    
+                }
+
+
+            }
+
+
+            public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<Message> argument)
+            {
+                var message = await argument;
+
+                if (message.Text == "reset")
+                {
+                    PromptDialog.Confirm(
+                        context,
+                        AfterResetAsync,
+                        "Are you sure you want to reset the count?",
+                        "Didn't get that!",
+                        promptStyle: PromptStyle.None);
+                }
+                else
+                {
+                    await context.PostAsync(string.Format("{0}: You said {1}", count++, message.Text));
+
+                    context.Wait(MessageReceivedAsync);
+                }
+            }
+
+            public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+            {
+                var confirm = await argument;
+                if (confirm)
+                {
+                    count = 1;
+
+                    await context.PostAsync("Reset count.");
+                }
+                else
+                {
+                    await context.PostAsync("Did not reset count.");
+                }
+                context.Wait(MessageReceivedAsync);
+            }
+        }
 }
