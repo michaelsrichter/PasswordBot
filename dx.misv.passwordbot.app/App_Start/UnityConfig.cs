6fe5f08940e0dcf9e9cd8ad61bd9c34258b29cd8
Mike Richter <richtercloud@outlook.com>
2016-07-05 17:39:28 -0400

    Remove unneeded references to core and others

--- dx.misv.passwordbot.app/App_Start/UnityConfig.cs
+++ dx.misv.passwordbot.app/App_Start/UnityConfig.cs
@@ -1,9 +1,7 @@
 using Microsoft.Practices.Unity;
 using System.Web.Http;
-using dx.misv.passwordbot.app.Controllers;
 using dx.misv.passwordbot.app.Dialogs;
 using dx.misv.passwordbot.app.Services;
-using dx.misv.passwordbot.core;
 using Unity.WebApi;
 
 namespace dx.misv.passwordbot.app
@@ -12,12 +10,7 @@ namespace dx.misv.passwordbot.app
     {
         public static void RegisterComponents()
         {
-            var container = RegisterUnityContainer.Get();
-
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-
-            // e.g. container.RegisterType<ITestService, TestService>();
+            var container = new UnityContainer();
             container.RegisterType<IWordService, ResourceWordService>(new ContainerControlledLifetimeManager());
             container.RegisterType(typeof(PasswordDialog));
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
